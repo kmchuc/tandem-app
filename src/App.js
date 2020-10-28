56e1518a22bdd7a7c9b933df3f369e3aa7bb1f1b
Kimberly Chuc <kimmchuc@gmail.com>
2020-10-27 15:59:14 -1000

    created a components folder to keep track of components used, created a TriviaQuestion component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TriviaQuestion from '../src/components/TriviaQuestion'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="#tandemtrivia">
+        <div className="questions">
+          <TriviaQuestion tagline="things are about to get trivial" />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/TriviaQuestion.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class TriviaQuestion extends React.Component {
+  render() {
+    return (
+      
+    )
+  }
+}
