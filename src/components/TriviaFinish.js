c9e61d557669c9d298ba87354dc84f0b42b920c7
Kimberly Chuc <kimmchuc@gmail.com>
2020-11-02 15:45:03 -1000

    finish page styling

--- src/components/TriviaFinish.js
+++ src/components/TriviaFinish.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TriviaFinish = ({score}) => {
   return(
-    <div>
+    <div className="finish">
       <h1>TriviaFinish</h1>
       <h2>Total Score: { score }</h2>
       <button onClick={e => window.location.reload()}>Replay</button>
